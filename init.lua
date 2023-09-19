9673122eb6def3ebc00da044d387b94fa2bf13cc
iamthebenja <ritterb0586@gmail.com>
2023-09-19 11:10:34 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -171,7 +171,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'catppuccin',
+        theme = 'rose-pine',
         component_separators = '|',
         section_separators = '',
       },

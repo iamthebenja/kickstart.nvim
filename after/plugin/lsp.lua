8fe7948fbedf05cfa52029abae6388d03c4fa446
iamthebenja <ritterb0586@gmail.com>
2023-10-08 22:09:58 -0400

    setup mason

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -81,6 +81,7 @@ capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 -- Ensure the servers above are installed
 local mason_lspconfig = require 'mason-lspconfig'
 
+require('mason').setup()
 mason_lspconfig.setup {
     ensure_installed = vim.tbl_keys(servers),
 }

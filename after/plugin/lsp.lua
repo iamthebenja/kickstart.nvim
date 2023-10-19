ab93620a8d7f5dd72cf9a3a9b41425401e74bfba
iamthebenja <ritterb0586@gmail.com>
2023-10-19 01:53:22 -0400

    update keymap

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -36,13 +36,6 @@ local on_attach = function(_, bufnr)
     nmap('<leader>wl', function()
         print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     end, '[W]orkspace [L]ist Folders')
-
-    -- Create a command `:Format` local to the LSP buffer
-    vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-        vim.lsp.buf.format()
-    end, { desc = 'Format current buffer with LSP' })
-
-    nmap('<leader>f', vim.lsp.buf.format(), '[F]ormat Code')
 end
 
 -- Enable the following language servers
